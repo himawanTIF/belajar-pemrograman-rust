bd28af6a248bc0a1de019e24fb60ae1ff68bdc4a
himawanTIF <himawan1700018032@webmail.uad.ac.id>
2025-07-07 21:16:49 +0700

    Happy day #1 Rust

--- halo_rust/src/main.rs
+++ halo_rust/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    println!("How");
+    println!("Are");
+    println!("You?");
 }
 // Pembuatan fungsi di rust menggunakan keyword fn
 // Notasi pemanggilan nama_fungsi();
 // Contoh: sebuah_fungsi("sebuah argument");
+// Pemanggilan fungsi dengan macro bernama println! bukan fungsi println
+// Pemanggilan macro ini ditandai dengan sintaksis !
+// Penulisan statement wajib diakhiri tanda ;
+// Standard indentasi untuk kode adalah 4 karakter spasi
---
+++ halo_rust/target/release/deps/halo_rust-f1acfe5f7e358f61.d
@@ -0,0 +1,5 @@
+/home/nitrozs/Documents/belajar-pemrograman-rust/halo_rust/target/release/deps/halo_rust-f1acfe5f7e358f61.d: src/main.rs
+
+/home/nitrozs/Documents/belajar-pemrograman-rust/halo_rust/target/release/deps/halo_rust-f1acfe5f7e358f61: src/main.rs
+
+src/main.rs:
